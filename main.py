7a13239ffe005291f1dc43b7a8c81e967d0499cd
TokenMaster Global <qianzhouxia@gmail.com>
2026-05-16 12:03:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-git add .
-git commit -m "v33.2 Stable: Integrated Gumroad & Fixing Database Error"
-git push origin main
+if __name__ == '__main__':
+    # 动态获取 Zeabur 注入的 PORT 环境变量，默认 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
